30f61d52b419e02a94c0640135171a80fa40e8b3
Herb Bowie <hbowie@mac.com>
2016-01-24 07:40:15 -0800

    Added isGoodInputFile static utility method
    Checks to see if a file exists, is a file, and is readable.

--- src/com/powersurgepub/psutils/FileUtils.java
+++ src/com/powersurgepub/psutils/FileUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 1999 - 2015 Herb Bowie
+ * Copyright 1999 - 2016 Herb Bowie
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -246,6 +246,13 @@ public class FileUtils {
     return (file != null
         && file.exists()
         && file.isDirectory()
+        && file.canRead());
+  }
+  
+  public static boolean isGoodInputFile(File file) {
+    return (file != null
+        && file.exists()
+        && file.isFile()
         && file.canRead());
   }
   
